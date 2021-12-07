47cf9bbabc57a26bef6427739edeaafe82d19d74
MD Dubey <mddubey409@gmail.com>
2021-12-07 11:55:24 +0530

    Accomodate both

--- 2021/007/solution.rb
+++ 2021/007/solution.rb
@@ -2,17 +2,24 @@ input = File.readlines('./007/input.txt')[0].split(',').map{|i| i.to_i}
 
 def calculate_cost_for(pos, numbers)
     numbers.reduce(0) do |cost, curr|                
-        diff = (curr - pos).abs        
+        diff = (curr - pos).abs      
+        #part1  
+        # cost += diff
+        #part2  
         cost += (diff * (diff+1))/2
         cost
     end
 end
 
-total_possible = (input.min..input.max).to_a
+def minimum_cost(input)
+    total_possible = (input.min..input.max).to_a
+    all_costs = total_possible.reduce({}) do |costs, pos|
+        costs[pos] = calculate_cost_for(pos, input)
+        costs
+    end
 
-costs = total_possible.reduce({}) do |costs, pos|
-    costs[pos] = calculate_cost_for(pos, input)
-    costs
+    return all_costs.sort_by {|_key, value| value}[0]
 end
 
-puts costs.sort_by {|_key, value| value}[0]
+puts minimum_cost(input)
+
